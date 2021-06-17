a617877991728ca40e5cb379664f445faae5a1c3
chocochipcook2 <jsy23232@naver.com>
2021-06-17 12:17:54 +0900

    client modified

--- node/server.js
+++ node/server.js
@@ -1,19 +1,24 @@
 var net = require('net');
 var server = net.createServer(function(client) {
-console.log("good")
-client.setTimeout(500);
-client.setEncoding('utf8');
-client.on('data', function(data) {
-writeData(client, 'Sending: ' + data.toString());
-});
-client.on('end', function() {
-server.getConnections(function(err, count){
-});
-});
-client.on('error', function(err) {
-});
-client.on('timeout', function() {
-});
+    console.log("good")
+    client.setTimeout(500);
+    client.setEncoding('utf8');
+    client.on('data', function(data) {
+        writeData(client, 'Sending: ' + data.toString());
+    });
+
+    client.on('end', function() {
+        server.getConnections(function(err, count){
+    }); 
+
+    });
+    client.on('error', function(err) {
+        console.log(err);
+    });
+
+    client.on('timeout', function() {
+    });
+
 });
 server.listen(8107, function() {
     console.log("listen,8107")
